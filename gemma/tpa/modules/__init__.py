a595cb5b4b3ecfe230ed1d57e36b247cc6ea90ef
rohan <rohanganapa@gmail.com>
2025-03-18 21:47:46 -0400

    thing

--- gemma/tpa/modules/__init__.py
+++ gemma/tpa/modules/__init__.py
@@ -5,13 +5,6 @@ This package provides specialized modules for Tensor Product Attention (TPA)
 implementations for Gemma3 models.
 """
 
-from .tucker_factorization import (
-    _factorize_mha_weights_with_shared_factors,
-    factorize_all_layers_with_shared_factors,
-    adaptive_rank_selection,
-    _factorize_and_set_weights
-)
-
 from .contextual_factorization import (
     contextual_tensor_decomposition,
     _init_contextual_factorization,
@@ -26,10 +19,6 @@ from .tensor_product_utils import (
 )
 
 __all__ = [
-    "_factorize_mha_weights_with_shared_factors",
-    "factorize_all_layers_with_shared_factors",
-    "adaptive_rank_selection",
-    "_factorize_and_set_weights",
     "contextual_tensor_decomposition",
     "_init_contextual_factorization",
     "apply_contextual_tensor_decomposition",
