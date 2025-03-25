b2dec83918350f845d4ccb360ce14f5b94272b90
rohan <rohanganapa@gmail.com>
2025-03-25 16:05:15 -0400

    thing

--- gemma/tpa/__init__.py
+++ gemma/tpa/__init__.py
@@ -1,3 +1,3 @@
 """Tensor Product Attention (TPA) implementations for Gemma models."""
 
-from .gemma3_tpa_model import Gemma3ForCausalLMwithTPA
+from .gemma3_tpa_model import GemmaForCausalLMwithTPA
