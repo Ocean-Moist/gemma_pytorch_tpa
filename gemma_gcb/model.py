1591b903a68bcfbd9ac9dd1fde5eb8e29a41e527
rohan <rohanganapa@gmail.com>
2025-04-27 23:06:58 -0400

    r

--- gemma_gcb/model.py
+++ gemma_gcb/model.py
@@ -34,8 +34,9 @@ class GemmaForCausalLM_GCB(torch.nn.Module):
 
         # ---------- Insert GCB heads + caches ---------------------
         meta = GCBMeta.load(meta_path)
-
+        r_k = next(iter(meta.heads.values())).P_r.shape[1]
         d_k, d_v = cfg.head_dim, cfg.head_dim      # Gemma uses same dim
+
         n_heads, max_seq = cfg.num_attention_heads, cfg.max_position_embeddings
         self.layer_caches: List[GCCache] = torch.nn.ModuleList()
 
