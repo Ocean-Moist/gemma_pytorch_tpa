94145824cc6609a55d25a19b28792a036457ef92
rohan <rohanganapa@gmail.com>
2025-05-02 15:07:31 -0400

    t

--- convert_weights.py
+++ convert_weights.py
@@ -119,6 +119,7 @@ def compute_asap_factors(
     )
 
     for layer_idx in range(n_layers):
+        print(state.keys())
         state = state["state_dict"]
         key_prefix = f"model.layers.{layer_idx}.self_attn.qkv_proj.weight"
         if key_prefix not in state:
