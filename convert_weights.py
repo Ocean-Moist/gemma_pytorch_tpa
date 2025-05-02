31a04b6dad28a620afd3a856c1ea1a07a4525f87
rohan <rohanganapa@gmail.com>
2025-05-02 15:15:04 -0400

    t

--- convert_weights.py
+++ convert_weights.py
@@ -119,12 +119,9 @@ def compute_asap_factors(
     )
 
     for layer_idx in range(n_layers):
-        print(type(state))
-        print(state.keys())
-        print(list(state.keys()))
-        state = state["model_state_dict"]
+        unwrapped_state = state["model_state_dict"]
         key_prefix = f"model.layers.{layer_idx}.self_attn.qkv_proj.weight"
-        if key_prefix not in state:
+        if key_prefix not in unwrapped_state:
             raise KeyError(f"expected key '{key_prefix}' in state_dict")
         W_qkv = state[key_prefix].float()                     # (out, in)
 
